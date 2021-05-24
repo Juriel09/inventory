8f1fc7d7e005a6e4947fe874c9788659d295f8e3
Juriel09 <duyejamilla@gmal.com>
2021-05-24 18:44:42 +0800

    first commit

---
+++ src/test/java/com/vanilla/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanilla.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
